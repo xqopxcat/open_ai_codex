3a75c89d318269531465be2fc97906e90e90a8a9
Steve Lee 李孟儒 <steve.lee@TWTPMAC10004148.local>
2023-01-07 18:29:31 +0800

    change baseUrl

--- client/src/redux/services/openAiCore.js
+++ client/src/redux/services/openAiCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const openAiCoreApi = createApi({
     reducerPath: 'openAiCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:8080',
+        baseUrl: 'https://open-ai-codex-5pom.onrender.com/',
         prepareHeaders: (headers) => {
             headers.set('Content-Type', 'application/json');
             return headers;
